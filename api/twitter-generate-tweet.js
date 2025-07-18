c99d384a132d161aebf5bc8e94bff4e05cb0f87d
turrs <faturrahmanavoiders@gmail.com>
2025-07-18 23:32:20 +0800

    cors extension

--- api/twitter-generate-tweet.js
+++ api/twitter-generate-tweet.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
     if (req.method !== 'POST') {
       return res.status(405).json({ error: 'Method not allowed' });
     }
